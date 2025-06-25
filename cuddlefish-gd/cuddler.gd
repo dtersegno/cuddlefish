e41d439c3b2e6094b8de16a81816adc071ad3e6b
David Tersegno <dtersegno@gmail.com>
2025-06-25 19:42:31 -0400

    grid

---
+++ cuddlefish-gd/cuddler.gd
@@ -0,0 +1,39 @@
+extends PanelContainer
+
+@onready var UL = $GridContainer/UL
+@onready var U = $GridContainer/U
+@onready var UR = $GridContainer/UR
+@onready var L = $GridContainer/L
+@onready var C = $GridContainer/C
+@onready var R = $GridContainer/R
+@onready var DL = $GridContainer/DL
+@onready var D = $GridContainer/D
+@onready var DR = $GridContainer/DR
+
+@onready var edge_blocks = [R, UR, U, UL, L, DL, D, DR]
+
+@onready var button = $Button
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	button.pressed.connect(self._on_button_pressed)
+	
+func _on_button_pressed():
+	rotate_block_colors()
+
+func rotate_block_colors():
+	var first_block_color = edge_blocks[0].color
+	for block_index in range(len(edge_blocks) - 1):
+		var this_block = edge_blocks[block_index]
+		var next_block = edge_blocks[block_index + 1]
+		this_block.color = next_block.color
+	edge_blocks[-1].color = first_block_color
+	return null
+
+func set_edge_block_colors(colors):
+	for index in range(len(edge_blocks)):
+		edge_blocks[index].color = colors[index]
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- cuddlefish-gd/grid.gd
+++ cuddlefish-gd/grid.gd
@@ -4,7 +4,7 @@ extends GridContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	create_cuddlefish()
+	create_cuddlefish(24)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

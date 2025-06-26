cde8ac38445a7c66f1b8a986ee826afd6ebc9d07
dtersegno <dtersegno@gmail.com>
2025-06-25 20:08:18 -0400

    rotation

--- cuddlefish-gd/cuddler.gd
+++ cuddlefish-gd/cuddler.gd
@@ -13,12 +13,40 @@ extends PanelContainer
 @onready var edge_blocks = [R, UR, U, UL, L, DL, D, DR]
 
 @onready var button = $Button
+
+
+var bw_colors =  [
+	Color(0,0,0),
+	Color(1,1,1),
+	Color(0,0,0),
+	Color(1,1,1),
+	Color(0,0,0),
+	Color(1,1,1),
+	Color(0,0,0),
+	Color(1,1,1)
+]
+
+var cuddle_colors = [
+	Color('DARK_ORCHID'),
+	Color('DARK_ORANGE'),
+	Color('HONEYDEW'),
+	Color('CHARTREUSE'),
+	Color('CORNFLOWER_BLUE'),
+	Color('DARK_ORCHID'),
+	Color('DARK_ORCHID'),
+	Color('HONEYDEW'),
+	Color('DARK_ORCHID')
+]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	button.pressed.connect(self._on_button_pressed)
+	set_edge_block_colors(cuddle_colors)
+	lighten_block_colors()
 	
 func _on_button_pressed():
 	rotate_block_colors()
+	#lighten_block_colors()
 
 func rotate_block_colors():
 	var first_block_color = edge_blocks[0].color
@@ -28,6 +56,10 @@ func rotate_block_colors():
 		this_block.color = next_block.color
 	edge_blocks[-1].color = first_block_color
 	return null
+
+func lighten_block_colors():
+	for block in edge_blocks:
+		block.color = block.color.lightened(0.2)
 
 func set_edge_block_colors(colors):
 	for index in range(len(edge_blocks)):
